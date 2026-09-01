b36a41701e49479d050ccd121f20bc1901e81a57
Marcel Dechant <marcel.dechant@freenet.de>
2026-09-01 14:59:49 +0200

    Update deployment configuration and header links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,42 @@
 import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './Header.module.css';
 
-
 export default function Header() {
+  const { siteConfig } = useDocusaurusContext();
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.navDiv}>
           <nav className={styles.nav}>
-            <a className={clsx(styles.navLink)} href="/#about">About me</a>
-            <a className={clsx(styles.navLink)} href="/#skills">My skills</a>
-            <a className={clsx(styles.navLink)} href="/#projects">My projects</a>
-            <a className={clsx(styles.navLink)} href="/#contact">Contact</a>
+            <a
+              className={clsx(styles.navLink)}
+              href={`${siteConfig.baseUrl}#about`}
+            >
+              About me
+            </a>
+            <a
+              className={clsx(styles.navLink)}
+              href={`${siteConfig.baseUrl}#skills`}
+            >
+              My skills
+            </a>
+            <a
+              className={clsx(styles.navLink)}
+              href={`${siteConfig.baseUrl}#projects`}
+            >
+              My projects
+            </a>
+            <a
+              className={clsx(styles.navLink)}
+              href={`${siteConfig.baseUrl}#contact`}
+            >
+              Contact
+            </a>
           </nav>
         </div>
       </div>
     </header>
-        );
+  );
 }
